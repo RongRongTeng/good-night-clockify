b73b510f9950d53ad5d01cff0657f37794e46767
Ya-Rong, Teng <yarong.teng@gmail.com>
2023-06-26 02:32:09 +0800

    feat: install apipei

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
     end
   end
 
+  apipie
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
