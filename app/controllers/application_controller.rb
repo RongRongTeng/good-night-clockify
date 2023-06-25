4f0e557c6fc7b99b8f73c1219774d517e2adfe0a
Ya-Rong, Teng <yarong.teng@gmail.com>
2023-06-25 20:27:33 +0800

    feat(controllers): authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include ActionController::MimeResponds
+
+  respond_to :json
+
+  before_action :authenticate_user!
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  defaults format: :json do
+    devise_for :users, only: :sessions
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
